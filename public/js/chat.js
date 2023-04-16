7d8d790366c54c71c00033f00d84546068916c63
Gourab Sanyal <sanyal.gourab@gmail.com>
2023-04-16 16:53:01 +0530

    location logic added

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,36 @@
 const socket = io();
 
-socket.on("countUpdated", (count) => {
-  console.log("Count has been updated ", count);
+socket.on("message", (message) => {
+  console.log(message);
 });
 
-document.querySelector("#increment").addEventListener("click", (count) => {
-  console.log("Clicked");
-  socket.emit("increment");
+document.querySelector("#message-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const message = e.target.elements.message.value;
+  // also can be done by "document.querySelector("input").value;"
+  socket.emit("sendMessage", message);
 });
+
+document.querySelector("#send-location").addEventListener("click", (e) => {
+  e.preventDefault();
+  if (!navigator.geolocation) {
+    return alert("Geo location is not supported by your browser");
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit("sendLocation", {
+      lat: position.coords.latitude,
+      long: position.coords.longitude,
+    });
+  });
+});
+
+// socket.on("countUpdated", (count) => {
+//   console.log("Count has been updated ", count);
+// });
+
+// document.querySelector("#increment").addEventListener("click", (count) => {
+//   console.log("Clicked");
+//   socket.emit("increment");
+// });
--- src/index.js
+++ src/index.js
@@ -14,29 +14,44 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(publicDirectoryPath, "index.html"));
-// });
-
-let count = 0;
-
-//  server (emit) -> client recieved - countUppdated
-// client (emit) -> server recieved - increment
-
 // 'socket'object holds the information about the new connection
 io.on("connection", (socket) => {
-  console.log("New socket connection");
+  console.log("Socket connected");
 
-  socket.emit("countUpdated", count);
+  // "welcome" to perticular user
+  socket.emit("message", "Welcome");
 
-  socket.on("increment", () => {
-    count++;
-    // this will update the value to one client
-    // socket.emit("countUpdated", count);
-
-    // this will update the value in all the clients
-    io.emit("countUpdated", count);
+  // emit to everybody but that perticular user
+  socket.broadcast.emit("message", "A new user has joined!");
+  socket.on("sendMessage", (message) => {
+    // send it to everyone
+    io.emit("message", message);
   });
+
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left!");
+  });
+
+  socket.on("sendLocation", (coords) => {
+    io.emit(
+      "message",
+      `https://google.com/maps?q=${coords.lat},${coords.long}`
+    );
+  });
+
+  //  server (emit) -> client recieved - countUppdated
+  // client (emit) -> server recieved - increment
+
+  // socket.emit("countUpdated", count);
+
+  // socket.on("increment", () => {
+  //   count++;
+  //   // this will update the value to one client
+  //   // socket.emit("countUpdated", count);
+
+  //   // this will update the value in all the clients
+  //   io.emit("countUpdated", count);
+  // });
 });
 
 server.listen(port, () => {
