8dd8ab3230f942b77dea54e5fc8f81925f0b8989
Gourab Sanyal <sanyal.gourab@gmail.com>
2023-04-16 12:18:14 +0530

    socket installed and logic updated

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+// socket expexts to be called raw http server
+// this logic - server supports the web sockets
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(publicDirectoryPath, "index.html"));
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(publicDirectoryPath, "index.html"));
+// });
+
+io.on("connection", () => {
+  console.log("New socket connection");
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log("Server is runnign on port " + port);
 });
