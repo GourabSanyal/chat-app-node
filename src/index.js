1500e75bd35684a0a81fd53b6ae488b858e5fbfa
Gourab Sanyal <sanyal.gourab@gmail.com>
2023-04-16 15:05:04 +0530

    example tutorial for socket connections

--- src/index.js
+++ src/index.js
@@ -18,8 +18,25 @@ app.use(express.static(publicDirectoryPath));
 //   res.sendFile(path.join(publicDirectoryPath, "index.html"));
 // });
 
-io.on("connection", () => {
+let count = 0;
+
+//  server (emit) -> client recieved - countUppdated
+// client (emit) -> server recieved - increment
+
+// 'socket'object holds the information about the new connection
+io.on("connection", (socket) => {
   console.log("New socket connection");
+
+  socket.emit("countUpdated", count);
+
+  socket.on("increment", () => {
+    count++;
+    // this will update the value to one client
+    // socket.emit("countUpdated", count);
+
+    // this will update the value in all the clients
+    io.emit("countUpdated", count);
+  });
 });
 
 server.listen(port, () => {
