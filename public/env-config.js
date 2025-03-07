94c5fcf2fb5f6aff5e89e39a4dc4b3a42991a681
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-03-07 12:03:37 +0530

    fs created local create-env js

---
+++ public/env-config.js
@@ -0,0 +1,5 @@
+
+window.BACKEND_URL = "http://localhost:3000";
+window.FRONTEND_URL = "http://localhost:8080";
+window.ENV = "development";
+console.log("Environment config loaded:", "development");
