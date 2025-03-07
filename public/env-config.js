c18bfc70ec52dd3f1fbb1fcb1895477df6fd3792
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-03-07 12:04:43 +0530

    local update backedn url for create-env

--- public/env-config.js
+++ public/env-config.js
@@ -1,5 +1,5 @@
 
 window.BACKEND_URL = "http://localhost:3000";
-window.FRONTEND_URL = "http://localhost:8080";
+window.FRONTEND_URL = "http://localhost:3000";
 window.ENV = "development";
 console.log("Environment config loaded:", "development");
